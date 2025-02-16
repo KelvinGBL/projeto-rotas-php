b8879f0bf0b32b23a84961aecd952024446061cb
Kelvin Gabriel <109625159+KelvinGBL@users.noreply.github.com>
2025-02-16 15:21:48 -0300

    Create Component Sidebar

---
+++ app/views/components/sidebar.php
@@ -0,0 +1,57 @@
+<div class="app-left">
+    <button class="close-menu">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/></svg>
+    </button>
+    <div class="app-logo">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2">
+        <line x1="18" y1="20" x2="18" y2="10"/>
+        <line x1="12" y1="20" x2="12" y2="4"/>
+        <line x1="6" y1="20" x2="6" y2="14"/>       </svg>
+    <span>Logo</span>
+    </div>
+    <ul class="nav-list">
+
+    <li id="nav-link-home" class="nav-list-item">
+        <a class="nav-list-link" href="home">
+            <?php require __DIR__ . "../../icons/home.svg"; ?>
+            Home
+        </a>
+    </li>
+
+    <li id="nav-link-dashboard" class="nav-list-item">
+        <a class="nav-list-link" href="dashboard">
+            <?php require __DIR__ . "../../icons/dashboard.svg"; ?>
+            Dashboard
+        </a>
+    </li>
+
+    <li class="nav-list-item">
+        <a class="nav-list-link" href="jobs">
+            <?php require __DIR__ . "../../icons/jobs.svg"; ?>
+            Jobs
+        </a>
+    </li>
+    
+    <li class="nav-list-item">
+        <a class="nav-list-link" href="applications">
+            <?php require __DIR__ . "../../icons/applications.svg"; ?>
+            Applications
+        </a>
+    </li>
+
+    <li class="nav-list-item">
+        <a class="nav-list-link" href="users">
+            <?php require __DIR__ . "../../icons/users.svg"; ?>
+            Users
+        </a>
+    </li>
+
+    <li class="nav-list-item">
+        <a class="nav-list-link" href="reports">
+            <?php require __DIR__ . "../../icons/reports.svg"; ?>
+            Reports
+        </a>
+    </li>
+
+    </ul>
+</div>
